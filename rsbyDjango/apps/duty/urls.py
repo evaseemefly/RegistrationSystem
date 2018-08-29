64ac80f74b49a71a1ca9776463428e2e9bea0805
wangbao <wangbao0801@outlook.com>
2018-08-29 09:22:21 +0800

    11

--- rsbyDjango/apps/duty/urls.py
+++ rsbyDjango/apps/duty/urls.py
@@ -6,16 +6,14 @@ from . import views
 app_name = 'duty'
 urlpatterns = [
     url(r'^list/$', views.DutyListView.as_view(), name='list'),
-    # 获取值班信息
     url(r'^schedulelist/$',views.ScheduleListView.as_view(),name='schedulelist'),
     url(r'^userlist/',views.UserListView.as_view(),name='userlist'),
     url(r'^dutylist/',views.DutyListView.as_view(),name="dutylist"),
     url(r'^modity/',views.ScheduleModificationView.as_view()),
     url(r'^grouplist/',views.GroupListView.as_view()),
-    # 创建值班信息
     url(r'^schedulelist/creat/$',views.ScheduleCreateView.as_view()),
     url(r'^schedulelist/del/$',views.ScheduleDelView.as_view()),
     url(r'^create/user$',views.CreateUserView.as_view()),
-    url(r'^demo/$',views.DutyDemoView.as_view()),
-    url(r'^schdeulelistshow/$',views.ScheduleShowListView.as_view())
+    url(r'^tempTestJson/$',views.tempTestJson),
+	url(r'^scheduleshowlist/',views.ScheduleShowListView.as_view())
 ]
